4cbe2be1cc81bb56230439e457fe4037f5004271
YoujingYu99 <yuyoujing@hotmail.com>
2022-01-05 17:43:13 +0000

    writing audio names matching

--- audio_excerpt.py
+++ audio_excerpt.py
@@ -5,24 +5,28 @@ import pandas as pd
 import numpy as np
 from containers import *
 
-fileDir = os.path.dirname(os.path.realpath('__file__'))
-print(fileDir)
+fileDir = os.path.dirname(os.path.realpath(__file__))
 
-def extract_audio(csv_path):
+
+def extract_audio(csv_path, audio_path):
     total_df = pd.read_csv(csv_path)
 
     for index, row in total_df.iterrows():
-        count = 0
         start_time = str(row['start_time'][:-1])
         end_time = str(row['end_time'][:-1])
         file_name = row['filename']
-        json_name = file_name.split('/')[-1]
+        json_name = file_name.split('\\')[-1]
+        audio_name = json_name.rsplit('.', 1)[0]
         sentence = row['sentence']
-        y, sr = librosa.load(file_name, sr=16000, duration=10)
-        audio_seg = y[start_time:end_time]
-        audio_excerpt_name = json_name + count
-        write(audio_excerpt_name, sr, audio_seg, normalized=False)
-        count += 1
+        audio_file_name = os.path.join(audio_path, audio_name + '.ogg')
+        try:
+            y, sr = librosa.load(audio_file_name, sr=16000, duration=10)
+            audio_seg = y[start_time:end_time]
+            audio_excerpt_name = json_name + start_time
+            write(audio_excerpt_name, sr, audio_seg, normalized=False)
+        except:
+            continue
+
 
 def write(audio_excerpt_name, sr, x, normalized=False):
     """numpy array to MP3"""
@@ -34,6 +38,12 @@ def write(audio_excerpt_name, sr, x, normalized=False):
     song = pydub.AudioSegment(y.tobytes(), frame_rate=sr, sample_width=2, channels=channels)
     audio_excerpt_name_path = os.path.join(fileDir, 'audio_excerpts', audio_excerpt_name) + '.mp3'
     song.export(audio_excerpt_name_path, format="mp3", bitrate="320k")
+
+csv_path = 'confidence_css.csv'
+audio_path = os.path.join(fileDir, 'audio')
+
+extract_audio(csv_path, audio_path)
+
 
 # def extract_segments(y, sr, segments):
 #     # compute segment regions in number of samples
